a55a50e20caf7fc84dbac19ff5991ce02c66a54d
Alexsandr Crist <pp89635075943@gmail.com>
2023-11-05 08:24:47 +0300

    detalis

--- WebBook/catalog/views.py
+++ WebBook/catalog/views.py
@@ -1,5 +1,5 @@
-from cgitb import html
-from django.shortcuts import render
+#from cgitb import html
+#rom django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
